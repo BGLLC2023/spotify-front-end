4f38443ba24d81ed1b56974df095b00d8a563465
Greg Lauray <greglauray@Gregs-MacBook-Pro-2.local>
2026-05-04 06:28:50 -0400

    hosting message

--- spotify-analytics/src/components/navbar.jsx
+++ spotify-analytics/src/components/navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
      
       {/* <img className="logo" src= {logo} alt="" /> */}
       <h1>Spotify Analytics</h1>
+      <h4 color="white">Free hosting please allow 30-60 seconds for data</h4>
       
     
     </nav>
